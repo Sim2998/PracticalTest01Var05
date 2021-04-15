655c559a3223886ae88c05ea533850e1cd0a8c99
Sim Bordanca <sim_bordanca@yahoo.com>
2021-04-15 11:01:32 +0300

    ex A

--- app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var05/PracticalTest01Var05MainActivity.java
+++ app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var05/PracticalTest01Var05MainActivity.java
@@ -3,12 +3,26 @@ package ro.pub.cs.systems.eim.practicaltest01var05;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class PracticalTest01Var05MainActivity extends AppCompatActivity {
+
+    private EditText text;
+    private Button left_top;
+    private Button right_top;
+    private Button center;
+    private Button left_bottom;
+    private Button right_bottom;
+    private String displaytext = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_practical_test01_var05_main);
+
+        left_top = (Button)findViewById(R.id.top_left_b);
+        right
+
     }
 }
